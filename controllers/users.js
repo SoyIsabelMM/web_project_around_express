fec843354b240e1d911157c2bce35fa3121da436
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-11-27 23:09:40 -0300

    fix: Arreglar el manejador de errores en las rutas, para que cuando haya un id invalido lo pueda detectar y notificar al usuario que el id no es válido

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,11 @@
+const { default: mongoose } = require('mongoose');
 const Users = require('../models/user');
 
 module.exports.getUsers = async (req, res) => {
   try {
     const users = await Users.find({});
+
+    console.log('traer todos', users);
 
     res.json({ data: users });
   } catch (err) {
@@ -17,15 +20,21 @@ module.exports.getUserById = async (req, res) => {
   const id = req.params._id;
 
   try {
-    const user = await User.findById(id);
+    const user = await Users.findById(id);
+
+    console.log(user);
 
     if (!user) {
       return res.status(404).json({ message: 'ID de usuario no encontrado' });
     }
 
     return res.json(user);
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    if (err instanceof mongoose.CastsError) {
+      return res.status(404).json({ message: 'ID de usuario no válido' });
+    }
+
+    console.error(err);
     return res
       .status(500)
       .json({ mensaje: 'Error al obtener usuario desde la base de datos' });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const { getUsers, getUserById } = require('../controllers/users');
+const mongoose = require('mongoose');
+
+const validateObjectId = (req, res, next) => {
+  const id = req.params._id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: 'ID de usuario no válido' });
+  }
+
+  next();
+};
 
 router.get('/users', getUsers);
-router.get('/users/:_id', getUserById);
+router.get('/users/:_id', validateObjectId, getUserById);
 
 module.exports = router;
