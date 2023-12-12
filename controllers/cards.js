41502663e0afecd45d0ab58defb5e5ff6276b404
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-11 21:11:23 -0300

    feat: Agregar solicitud delete para eliminar card de la bd

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Cards = require('../models/card');
 const User = require('../models/user');
 
@@ -30,5 +31,26 @@ module.exports.createCard = async (req, res) => {
     return res
       .status(500)
       .json({ message: 'Error al crear una nueva tarjeta' });
+  }
+};
+
+module.exports.deleteCard = async (req, res) => {
+  const cardId = req.params.cardId;
+
+  if (!mongoose.Types.ObjectId.isValid(cardId)) {
+    return res.status(400).json({ message: 'ID de tarjeta invalido' });
+  }
+
+  try {
+    const result = await Cards.findByIdAndDelete(cardId);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Tarjeta no encontrada' });
+    }
+
+    return res.status(200).json({ message: 'Tarjeta eliminada exitosamente' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Error interno del servidor' });
   }
 };
